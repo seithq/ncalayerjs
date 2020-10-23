8db8a376755a31f5567b8dcd7dff725b3a584740
danikarik <daniyar@hey.com>
2020-10-24 03:14:56 +0600

    Add Client

---
+++ src/__tests__/client.test.ts
@@ -0,0 +1,11 @@
+import Client from "../client"
+
+test("should create client", () => {
+  const client = new Client("wss://127.0.0.1:13579/")
+  expect(client.hasError()).toBe(false)
+})
+
+test("should create client with empty url", () => {
+  const client = new Client()
+  expect(client.hasError()).toBe(false)
+})
---
+++ src/client.ts
@@ -0,0 +1,107 @@
+type NCALayerError = {
+  code: number
+  reason: string
+  message: string
+}
+
+enum MethodName {
+  BrowseKeyStore = "browseKeyStore",
+  ShowFileChooser = "showFileChooser",
+  GetKeys = "getKeys",
+  SetLocale = "setLocale",
+  GetNotBefore = "getNotBefore",
+  GetNotAfter = "getNotAfter",
+  GetSubjectDN = "getSubjectDN",
+  GetIssuerDN = "getIssuerDN",
+  GetRdnByOid = "getRdnByOid",
+  SignPlainData = "signPlainData",
+  VerifyPlainData = "verifyPlainData",
+  CreateCMSSignature = "createCMSSignature",
+  VerifyCMSSignature = "verifyCMSSignature",
+  CreateCMSSignatureFromFile = "createCMSSignatureFromFile",
+  VerifyCMSSignatureFromFile = "verifyCMSSignatureFromFile",
+  SignXml = "signXml",
+  VerifyXml = "verifyXml",
+  SignXmlByElementId = "signXmlByElementId",
+  VerifyXmlByElementId = "verifyXmlByElementId",
+  GetHash = "getHash",
+}
+
+type Payload = {
+  method: MethodName
+  args: any[]
+}
+
+class Client {
+  private ws: WebSocket
+  private err: NCALayerError
+  private cb: (data) => {}
+
+  constructor(url?: string) {
+    this.err = { code: -1, reason: "", message: "" }
+    this.connect(url || "wss://127.0.0.1:13579/")
+  }
+
+  // ===========================================================================
+  // HELPERS
+  // ===========================================================================
+
+  private connect(url: string): void {
+    this.ws = new WebSocket(url)
+
+    this.ws.onopen = (e) => {}
+
+    this.ws.onclose = (e) => {
+      this.err = e.wasClean
+        ? { code: -1, reason: "", message: "" }
+        : { code: e.code, reason: e.reason, message: "connection failed" }
+    }
+
+    this.ws.onmessage = (e) => {
+      if (e.data === "--heartbeat--") {
+        return
+      }
+
+      this.cb(JSON.parse(e.data))
+    }
+  }
+
+  private send(data: Payload): void {
+    this.ws.send(JSON.stringify(data))
+  }
+
+  state(): number {
+    return this.ws.readyState
+  }
+
+  hasError(): boolean {
+    return this.err.code != -1 || this.err.message != ""
+  }
+
+  lastError(): NCALayerError {
+    return this.err
+  }
+
+  reconnect(): void {
+    this.connect(this.ws.url)
+  }
+
+  // ===========================================================================
+  // API
+  // ===========================================================================
+
+  browseKeyStore(
+    storageName: string,
+    fileExtension: string,
+    currentDirectory: string,
+    callback: (data) => {}
+  ): void {
+    this.cb = callback
+    this.send({
+      method: MethodName.BrowseKeyStore,
+      args: [storageName, fileExtension, currentDirectory],
+    })
+  }
+}
+
+export default Client
